e5d922335ee69b525d9f8356e674fb9cc49ff83c
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2014-05-03 11:55:08 +1200

    Replace arch-dependent integer byteswap routines with gcc builtins

--- headers/os/support/ByteOrder.h
+++ headers/os/support/ByteOrder.h
@@ -34,9 +34,9 @@ typedef enum {
 /* always swap macros */
 #define B_SWAP_DOUBLE(arg)   __swap_double(arg)
 #define B_SWAP_FLOAT(arg)    __swap_float(arg)
-#define B_SWAP_INT64(arg)    __swap_int64(arg)
-#define B_SWAP_INT32(arg)    __swap_int32(arg)
-#define B_SWAP_INT16(arg)    __swap_int16(arg)
+#define B_SWAP_INT64(arg)    __builtin_bswap64(arg)
+#define B_SWAP_INT32(arg)    __builtin_bswap32(arg)
+#define B_SWAP_INT16(arg)    __builtin_bswap16(arg)
 
 #if BYTE_ORDER == __LITTLE_ENDIAN
 /* Host is little endian */
@@ -61,16 +61,16 @@ typedef enum {
 /* Host native to big endian */
 #define B_HOST_TO_BENDIAN_DOUBLE(arg)	__swap_double(arg)
 #define B_HOST_TO_BENDIAN_FLOAT(arg)	__swap_float(arg)
-#define B_HOST_TO_BENDIAN_INT64(arg)	__swap_int64(arg)
-#define B_HOST_TO_BENDIAN_INT32(arg)	__swap_int32(arg)
-#define B_HOST_TO_BENDIAN_INT16(arg)	__swap_int16(arg)
+#define B_HOST_TO_BENDIAN_INT64(arg)	__builtin_bswap64(arg)
+#define B_HOST_TO_BENDIAN_INT32(arg)	__builtin_bswap32(arg)
+#define B_HOST_TO_BENDIAN_INT16(arg)	__builtin_bswap16(arg)
 
 /* Big endian to host native */
 #define B_BENDIAN_TO_HOST_DOUBLE(arg)	__swap_double(arg)
 #define B_BENDIAN_TO_HOST_FLOAT(arg)	__swap_float(arg)
-#define B_BENDIAN_TO_HOST_INT64(arg)	__swap_int64(arg)
-#define B_BENDIAN_TO_HOST_INT32(arg)	__swap_int32(arg)
-#define B_BENDIAN_TO_HOST_INT16(arg)	__swap_int16(arg)
+#define B_BENDIAN_TO_HOST_INT64(arg)	__builtin_bswap64(arg)
+#define B_BENDIAN_TO_HOST_INT32(arg)	__builtin_bswap32(arg)
+#define B_BENDIAN_TO_HOST_INT16(arg)	__builtin_bswap16(arg)
 
 #else	/* BYTE_ORDER */
 /* Host is big endian */
@@ -81,16 +81,16 @@ typedef enum {
 /* Host native to little endian */
 #define B_HOST_TO_LENDIAN_DOUBLE(arg)	__swap_double(arg)
 #define B_HOST_TO_LENDIAN_FLOAT(arg)	__swap_float(arg)
-#define B_HOST_TO_LENDIAN_INT64(arg)	__swap_int64(arg)
-#define B_HOST_TO_LENDIAN_INT32(arg)	__swap_int32(arg)
-#define B_HOST_TO_LENDIAN_INT16(arg)	__swap_int16(arg)
+#define B_HOST_TO_LENDIAN_INT64(arg)	__builtin_bswap64(arg)
+#define B_HOST_TO_LENDIAN_INT32(arg)	__builtin_bswap32(arg)
+#define B_HOST_TO_LENDIAN_INT16(arg)	__builtin_bswap16(arg)
 
 /* Little endian to host native */
 #define B_LENDIAN_TO_HOST_DOUBLE(arg)	__swap_double(arg)
 #define B_LENDIAN_TO_HOST_FLOAT(arg)	__swap_float(arg)
-#define B_LENDIAN_TO_HOST_INT64(arg)	__swap_int64(arg)
-#define B_LENDIAN_TO_HOST_INT32(arg)	__swap_int32(arg)
-#define B_LENDIAN_TO_HOST_INT16(arg)	__swap_int16(arg)
+#define B_LENDIAN_TO_HOST_INT64(arg)	__builtin_bswap64(arg)
+#define B_LENDIAN_TO_HOST_INT32(arg)	__builtin_bswap32(arg)
+#define B_LENDIAN_TO_HOST_INT16(arg)	__builtin_bswap16(arg)
 
 /* Host native to big endian */
 #define B_HOST_TO_BENDIAN_DOUBLE(arg)	(double)(arg)
@@ -111,7 +111,7 @@ typedef enum {
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 extern status_t swap_data(type_code type, void *data, size_t length,
 	swap_action action);
@@ -121,9 +121,6 @@ extern bool is_type_swapped(type_code type);
 /* Private implementations */
 extern double __swap_double(double arg);
 extern float  __swap_float(float arg);
-extern uint64 __swap_int64(uint64 arg);
-extern uint32 __swap_int32(uint32 arg);
-extern uint16 __swap_int16(uint16 arg);
 
 #ifdef __cplusplus
 }
